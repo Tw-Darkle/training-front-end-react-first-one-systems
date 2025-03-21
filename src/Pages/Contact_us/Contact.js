202fdcc2c03a042e631ed58cdd634294b40b5edc
Tw-Darkle <freedom.tai.130@gmail.com>
2025-03-21 18:26:11 +0700

    write content contact

--- src/Pages/Contact_us/Contact.js
+++ src/Pages/Contact_us/Contact.js
@@ -1,6 +1,33 @@
+import "./contact.css";
+
 const Contact = () => {
-    return (
-        <div>Home Page</div>
-    )
-}
-export default Contact
+  return (
+    <div className="contact-content">
+      <div className="header-content">
+        <div className="header">
+          <div className="boxs"></div>
+          <div className="text-p">
+            <p className="title-header"> CONTACT US</p>
+            <span>
+              <a href="/">Home</a> / <a href="/Contact">Contact</a>
+            </span>
+          </div>
+          <img
+            src="https://www.firstone.co.th/public/assets/app/img/bg/13.jpg"
+            alt="bg-header"
+          />
+        </div>
+        <div className="body-content">
+          <img src="https://www.firstone.co.th/public/assets/app/img/content/05.jpg" alt="body-image" />
+          <div className="text-body">
+            <p className="content-body-headder">FIRST ONE SYSTEMS <span>COMPANY LIMITED</span></p>
+            <p>719 KPN Tower 11th floor ,Rama 9 Road, Bangkapi, Huay-Kwang,
+            Bangkok 10310 Thailand</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contact;
